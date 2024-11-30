54732c8e305a9685a1166d0ff9aba3f403aea66e
axelchr12 <axelchristopher12@gmail.com>
2024-11-30 15:05:09 +0700

    feat: install icons and headlessui react

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,13 @@
+import { Modal } from '@/components/modals/Modal';
 import { WEB_INFO } from '@/core/util/constants';
 
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <h1>Home</h1>
+      <Modal />
+    </>
+  );
 };
 
 export default HomePage;
